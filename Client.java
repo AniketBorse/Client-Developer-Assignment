4657d17a6ff74bf28bdce156c1914c4b1c33c119
AniketBorse <92010290+AniketBorse@users.noreply.github.com>
2023-10-18 20:20:58 +0530

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package clientpro;
+import devpack.Myclass1();
+/**
+ * @author DELL
+ *
+ */
+public class ClientApp {
+	public static void main String[] args) {
+		Myclass1 m1 = new Myclass1();
+	}
+}
---
+++ Developer.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package devpack;
+
+/**
+ * @author Anni
+ *
+ */
+public class Myclass1 {
+	int num;
+
+	/**
+	 * @return the num
+	 */
+	public int getNum() {
+		return num;
+	}
+
+	/**
+	 * @param num the num to set
+	 */
+	public void setNum(int num) {
+		this.num = num;
+	}
+	
+	public Myclass1() {
+		this.num = num;
+	}
+}
